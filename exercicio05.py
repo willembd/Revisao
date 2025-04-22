eaa9268996213d487ab5e76e26bf7ab71c05f2db
Willemberg Bruno <willem.bruno@hotmail.com>
2025-04-22 09:30:59 -0300

    exercicio05

---
+++ exercicio05.py
@@ -0,0 +1,10 @@
+salmin = float(input("Qual o salário mínimo atual: "))
+sala = float(input("Qual é o seu Salário: "))
+div = sala/salmin
+
+"""cont=0
+while sala > salmin:
+    sala = sala - salmin
+    cont= cont+1"""
+
+print(f"Você recebe {div} salários minimos")

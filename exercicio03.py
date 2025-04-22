f7edffd712f70961037a89bdb0fd0b0ca0bf7f26
Willemberg Bruno <willem.bruno@hotmail.com>
2025-04-22 09:04:21 -0300

    exercicio03

---
+++ exercicio03.py
@@ -0,0 +1,14 @@
+v = "s"
+while v == "s":
+    n = int(input("Digite um número: "))
+
+    if n % 2 == 0 and n > 0:
+        print(f"O {n} é Par e Positivo")
+    elif n % 2 == 0 and n < 0:
+        print(f"O {n} é Par e Negativo")
+    elif n % 2 == 1 and n > 0:
+        print(f"O {n} é Impar e Positivo")
+    else:
+        print(f"O {n} é Impar e Negativo")
+
+    v = input("Deseja fazer novamente? s = sim / n = não: ")

6402591d9b8f5545a40101923df7ec4d7236124f
Willemberg Bruno <willem.bruno@hotmail.com>
2025-04-22 08:29:34 -0300

    exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,11 @@
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+c = int(input("Digite o valor de C: "))
+
+soma = a+b
+
+print(f"A soma dos dois números é: {soma}")
+
+if soma < c:
+    print("A soma de A + B é menor que C")
+
